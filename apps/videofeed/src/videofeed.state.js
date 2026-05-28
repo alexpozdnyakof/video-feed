3941b00ca00322df10f8cf2366994ff8f31e0496
alexpozdnyakof <alexpozdnyakof@gmail.com>
2026-05-29 00:41:24 +0700

    refactor(apps/videofeed): overwrite app logic to generator stream

--- apps/videofeed/src/videofeed.state.js
+++ apps/videofeed/src/videofeed.state.js
@@ -1,8 +1,9 @@
 import { videoFeedApi } from "./videofeed.api";
+const CHUNK_SIZE = 6;
 
 /**
  * @param {string} apiUrl
- * @returns {AsyncGenerator<{type: string; state: {active: number; videos: any[]}}>}
+ * @returns {AsyncGenerator<{type: string; state: any}>}
  */
 export async function* videoFeedState(apiUrl, events) {
   const videoChunk = videoFeedApi(apiUrl);
@@ -19,10 +20,67 @@ export async function* videoFeedState(apiUrl, events) {
   yield { type: "init", state };
 
   for await (const event of events) {
+    console.log({ event });
     switch (event.type) {
       case "fetch": {
+        console.log("fetch");
         const { value: chunk, done } = await videoChunk.next();
-        yield { type: "chunk", state: { active: state.active, videos: chunk } };
+        state = {
+          ...state,
+          active: state.active,
+          videos: [...state.videos, ...chunk],
+        };
+        console.log(state, "fetch update");
+        yield {
+          type: "chunk",
+          state,
+        };
+        break;
+      }
+      case "scroll": {
+        const nextIdx = event.payload.activeIdx;
+        if (nextIdx !== state.active) {
+          const type = nextIdx > state.active ? "scroll_down" : "scroll_up";
+          if (type === "scroll_down" && nextIdx < state.videos.length - 1) {
+            if (state.active > 0) {
+              const detachIdx = state.active - 1;
+              const attachIdx = detachIdx + 4;
+              yield {
+                type: "attach",
+                state: { idx: attachIdx, video: state.videos[attachIdx] },
+              };
+              yield { type: "detach", state: { idx: detachIdx } };
+            }
+
+            if (state.videos.length - nextIdx - 1 === CHUNK_SIZE / 2) {
+              const { value: chunk, done } = await videoChunk.next();
+              if (!done) {
+                state = {
+                  ...state,
+                  active: state.active,
+                  videos: [...state.videos, ...chunk],
+                };
+
+                yield {
+                  type: "chunk",
+                  state: { active: state.active, videos: chunk },
+                };
+              }
+            }
+          }
+
+          if (
+            (type === "scroll_down" && nextIdx < state.videos.length) ||
+            (type === "scroll_up" && state.active !== 0)
+          ) {
+            yield {
+              type: "scroll",
+              state: { prev: state.active, next: nextIdx },
+            };
+            state = { ...state, active: nextIdx };
+          }
+        }
+        break;
       }
     }
   }
